e2216190afb768a447f8bfd353427d4665e9042a
JC <justcme@gmail.com>
2017-09-09 17:32:01 +1000

    Added Colemak layer

--- keyboards/xd60/keymaps/jhychan/keymap.c
+++ keyboards/xd60/keymaps/jhychan/keymap.c
@@ -22,13 +22,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT,  _____,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  _____,    KC_RSFT,  _____,    \
     KC_LGUI,  MO(_FL),  KC_LALT,                                KC_SPC,                                 KC_RGUI,  MO(_FL),  _____,    KC_RALT,  TG(_ML)   ),
 
-  // Mac OS Base layer
+  // Colemak Base layer
   [_ML] = KEYMAP(
     __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    \
-    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,              __v__,    \
-    KC_LGUI,  __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    _____,              __v__,    \
-    __v__,    _____,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    __v__,    _____,    __v__,    _____,    \
-    KC_LCTL,  __v__,    __v__,                                  __v__,                                  KC_RCTL,  __v__,    _____,    __v__,    __v__     ),
+    __v__,    __v__,    __v__,    KC_F,     KC_P,     KC_G,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  __v__,    __v__,              __v__,    \
+    __v__,    __v__,    KC_R,     KC_S,     KC_T,     KC_D,     __v__,    KC_N,     KC_E,     KC_I,     KC_O,     __v__,    _____,              __v__,    \
+    __v__,    _____,    __v__,    __v__,    __v__,    __v__,    KC_B,     KC_K,     __v__,    __v__,   __v__,     __v__,    _____,    __v__,    _____,    \
+    __v__,    __v__,    __v__,                                  __v__,                                  __v__,    __v__,    _____,    __v__,    __v__     ),
 
   // Universal Function layer
   [_FL] = KEYMAP(
