ccfb94793ef8862e641029a7099d3e16438a2161
jhychan <justcme@gmail.com>
2018-02-19 00:29:01 +0000

    Switch to r_alt

--- keyboards/xd60/keymaps/jhychan/keymap.c
+++ keyboards/xd60/keymaps/jhychan/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_BSPC,  \
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  _____,              KC_ENT,   \
     KC_LSFT,  _____,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  _____,    KC_RSFT,  _____,    \
-    KC_NUHS,  MO(_FN),  KC_LALT,                                KC_SPC,                                 KC_RGUI,  MO(_FN),  _____,    KC_RALT,  TG(_GL)   ),
+    KC_NUHS,  MO(_FN),  KC_RALT,                                KC_SPC,                                 KC_RGUI,  MO(_FN),  _____,    KC_RALT,  TG(_GL)   ),
 
   // Guest mode layer
   [_GL] = KEYMAP(
