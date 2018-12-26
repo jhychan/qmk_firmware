aad3970236df60ec8bb0e626c6bfa2633ed7934a
Justin Chan <justcme@gmail.com>
2018-12-27 10:47:16 +1100

    Update left alt

--- keyboards/dz60/keymaps/jhychan/keymap.c
+++ keyboards/dz60/keymaps/jhychan/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_BSPC,  \
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   \
     KC_LSPO,  KC_LSPO,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSPC,  KC_RSPC,  \
-    _____,    MO(_FN),  KC_RALT,            KC_SPC,             KC_APP,             KC_SPC,             KC_RGUI,  MO(_FN),  _____,    TG(_GL),  _____     ),
+    _____,    MO(_FN),  KC_LALT,            KC_SPC,             KC_APP,             KC_SPC,             KC_RGUI,  MO(_FN),  _____,    TG(_GL),  _____     ),
 
   // Guest mode layer
   [_GL] = LAYOUT(
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     __v__,    _____,    _____,    KC_UP,    _____,    _____,    _____,    _____,    KC_UP,    _____,    KC_PSCR,  _____,    _____,              KC_DEL,   \
     __v__,    _____,    KC_LEFT,  KC_DOWN,  KC_RIGHT, _____,    _____,    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_HOME,  KC_PGUP,                      __v__,    \
     __v__,    __v__,    _____,    KC_APP,   _____,    _____,    _____,    _____,    _____,    _____,    KC_END,   KC_PGDN,            __v__,    __v__,    \
-    _____,    __v__,    __v__,              __v__,              KC_CAPS,            _____,              __v__,    __v__,    _____,    __v__,    _____     ),
+    _____,    __v__,    KC_LALT,            __v__,              KC_CAPS,            _____,              __v__,    __v__,    _____,    __v__,    _____     ),
 
   // Universal Control layer
   [_CL] = LAYOUT(
